67b1ce8865c9d985245e3fbfffc0349ef0984d9a
ReactOne <kamran.mahar@gmail.com>
2020-09-27 11:58:36 -0700

    servre

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const Router = express.Router();
-const productRoutes = require("./api/routes/products");
+//const productRoutes = require("./api/routes/products");
 const app = express();
 
 app.use(bodyParser.json());
 
-app.use("/products",productRoutes);
+//app.use("/products",productRoutes);
 
-
+Router.get('/', function(req, res) {
+    res.json({ message: 'API is Online!' });   
+});
+app.use('/api', Router);
 app.listen(3000);
+
+
+console.log('Listening on port ' );
