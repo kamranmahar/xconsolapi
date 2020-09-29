37ceb1485a5b1cc79b37430345110bde63992647
Kamran Mahar <kamran.mahar@gmail.com>
2020-09-29 12:41:22 +0500

    Update connection.js

--- api/routes/connection.js
+++ api/routes/connection.js
@@ -1,6 +1,7 @@
 const mysql = require("mysql");
 var mySqlConnection = mysql.createConnection({
     host : "34.67.30.151",
+    protocol : "TCP",
     port: "3306",
     user :"kamran",
     password : "Kamran@1234",
