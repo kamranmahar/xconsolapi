46b0652059d3f537bdc8287f50b79abd3eeb7771
Kamran Mahar <kamran.mahar@gmail.com>
2020-09-29 10:52:17 +0500

    Update brand.sql

--- brand.sql
+++ brand.sql
@@ -1,16 +1,7 @@
 CREATE TABLE `Brands` (
   `ID` int NOT NULL,
   `Name` varchar(250) NOT NULL,
-  `Description` varchar(500),
+  `Description` varchar(500) NOT NULL,
   `Slug` varchar(250) NOT NULL,
-  `Thumbnail` varchar(250)DEFAULT NULL
-) ;
-
---
--- Dumping data for table `Brands`
---
-
-INSERT INTO `Brands` (`ID`, `Name`, `Description`, `Slug`, `Thumbnail`) VALUES
-(1, 'Nokia', NULL, 'nokia', NULL),
-(2, 'Niki', NULL, 'niki', NULL),
-(3, 'Gucci', NULL, 'gucci', NULL);
+  `Thumbnail` varchar(250) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
